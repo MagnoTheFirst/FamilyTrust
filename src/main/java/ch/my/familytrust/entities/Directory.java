2982b0867f122ec10aabca853119ffe6c051468f
MagnoTheFirst <alejandro.r.laneri@gmail.com>
2025-04-01 19:07:12 +0200

    refactored transaction to stock

--- src/main/java/ch/my/familytrust/entities/Directory.java
+++ src/main/java/ch/my/familytrust/entities/Directory.java
@@ -1,4 +1,28 @@
 package ch.my.familytrust.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Directory {
+
+    @Id
+    public UUID id = UUID.randomUUID();
+
+    public String name;
+
+    public String description;
+
+    public LocalDate creationDate;
+
+    //TODO[] implement owner once user management is implemented
 }
